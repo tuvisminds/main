3f29a3595413a607dbae18cf906db438add0b9f8
SK <shrinivask@vitinfotech.com>
2025-07-12 22:16:29 +0530

    assetPrefix check in

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/main' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/main' : '',
   trailingSlash: true,
   images: {
     unoptimized: true
