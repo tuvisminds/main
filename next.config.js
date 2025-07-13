6377b475b450b23373d63662dc516c7eb776101d
SK <shrinivask@vitinfotech.com>
2025-07-13 06:48:21 +0530

    Lets check with basepath removed too.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/main' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/main' : '',
+  // basePath: process.env.NODE_ENV === 'production' ? '/main' : '',
+  // assetPrefix: process.env.NODE_ENV === 'production' ? '/main' : '',
   trailingSlash: true,
   images: {
     unoptimized: true
